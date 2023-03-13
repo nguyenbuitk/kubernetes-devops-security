47225ead7af2247eef7888a367ec898a130f4503
nguyenbuitk <buinguyen23112k@gmail.com>
2023-03-13 04:10:37 +0000

    post action in jenkins

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
-# openjdk
-# openjdk:8-alpine
-# openjdk:8-jdk-alpine
-# adoptopenjdk/openjdk8:alpine-slim
+# openjdk   0
+# openjdk:8       200 vulneribility
+# openjdk:8-alpine  200
+# openjdk:8-jdk-alpine  40
+# adoptopenjdk/openjdk8:alpine-slim 0
 
 dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
 echo $dockerImageName
@@ -11,8 +11,12 @@ whoami
 pwd
 
 # Nếu tìm thấy HIGH severity thì return 0, critical return 1
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity HIGH --light $dockerImageName
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.18.3 -q image --exit-code 0 --severity HIGH --light $dockerImageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.18.3 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
+
+# docker run --rm -v /var/run/docker.sock:/var/run/docker.sock \
+#      -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity HIGH --light $dockerImageName
+
 
     # Trivy scan result processing
     exit_code=$?
