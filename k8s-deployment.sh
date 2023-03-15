f24a67eb0b112e2840dfb6e7afb35386f7d096f2
nguyenbuitk <buinguyen23112k@gmail.com>
2023-03-15 15:49:21 +0000

    test rollout status of kubernetes

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# file này kiểm tra k8s deployment có tồn tại không, nếu có thì update image của container. nếu không thì deploy mới
-
+#
 sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
 kubectl -n default get deployment ${deploymentName} > /dev/null
 
-# nếu output command = 1 (tức là error không tìm thấy deployment)
-if [[ $? -ne 0]]; then
-    echo "deployment ${deploymentName} doesn't exist"
+#  output command = 1
+if [[ $? -ne 0 ]]; then
+    echo "deployment ${deploymentName} doesnt exist"
     kubectl -n default apply -f k8s_deployment_service.yaml
 else
     echo "deployment ${deployment} exist"
