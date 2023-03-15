da54e356dd2cb2e2b085e0d42a7f9f78d2011d5e
nguyenbuitk <buinguyen23112k@gmail.com>
2023-03-15 15:53:11 +0000

    test rollout status of kubernetes

--- k8s-deployment-rollout-status.sh
+++ k8s-deployment-rollout-status.sh
@@ -2,10 +2,9 @@
 
 sleep 60s
 
-if [[ $(kubectl -n default rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"*]];
-then
+if [[ $(kubectl -n default rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]]; then
     echo "Deployment ${deploymentName} Rollout has Failed"
-    kubectl -n default rollout undo deploy ${deployName}
+    kubectl -n default rollout undo deploy ${deploymentName}
     exit 1;
 
 else
