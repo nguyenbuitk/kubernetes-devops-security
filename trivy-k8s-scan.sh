b779d1cfbb9c9954fca97e82b631c2422c8d14ac
nguyenbuitk <buinguyen23112k@gmail.com>
2023-03-15 19:17:06 +0000

    kubesec parallel scan

--- trivy-k8s-scan.sh
+++ trivy-k8s-scan.sh
@@ -3,14 +3,23 @@
 
 echo $imageName
 docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $imageName | grep "CRITICAL"
+trivy_output=$(docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $imageName)
 
-    exit_code=$?
-    echo "Exit Code : $exit_code"
+num_critical=$(echo "$trivy_output" | grep -oP 'CRITICAL:\s*\K\d+')
 
-    if [[ ${exit_code} == 1 ]]; then
-        echo "Image scanning fail. Vulnerability found"
-        exit 1;
-    else
-        echo "Image scanning passed. No vulnerabilities found"
-    fi;
+# Output the number of critical vulnerabilities
+echo "Total: $num_critical (CRITICAL: $num_critical)"
+
+# Output the list of critical vulnerabilities
+echo "$trivy_output"
+
+
+    # exit_code=$?
+    # echo "Exit Code : $exit_code"
+
+    # if [[ ${exit_code} == 1 ]]; then
+    #     echo "Image scanning fail. Vulnerability found"
+    #     exit 1;
+    # else
+    #     echo "Image scanning passed. No vulnerabilities found"
+    # fi;
